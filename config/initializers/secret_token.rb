7c8166983ad924657199bc9d77753840bfda160a
justkrys1 <justkrys1@gmail.com>
2014-02-01 12:32:13 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codespork::Application.config.secret_key_base = '7aca13a7704541a5f22fdcc8c4695f64d4739104c810c28241e08b7c0fb6c7a768acf21d6f391a468e10440718bdf7f67447cd0989e987d630b177ce839fe998'
